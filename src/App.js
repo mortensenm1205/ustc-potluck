7df4c0fb47e56b4b0058571caabedd5aaca4f549
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2019-01-17 15:57:20 -0700

    list comp now broken down for css grid

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { AppContainer } from './App_css';
 import Form from './components/Form/';
 import Items from './components/Items/';
-import List from './components/List';
+import List from './components/List/';
 
 class App extends Component {
   render() {
---
+++ src/components/List/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { List } from './List_css';
+
+const ListContainer = () => {
+    return(
+        <List>
+            <p>What's left to bring: </p>
+        </List>
+    )
+}
+
+export default ListContainer;
