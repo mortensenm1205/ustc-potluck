03949993b88d6d293eca691b871092815304230b
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2019-01-17 16:30:19 -0700

    small base styles added

--- src/App_css.js
+++ src/App_css.js
@@ -4,4 +4,6 @@ export const AppContainer = styled.div`
     display: grid;
     grid-template-columns: repeat(12, 1fr);
     grid-template-rows: repeat(4, 1fr);
+    font-size: 18px;
+    font-family: 'Roboto', sans-serif;
 `;
