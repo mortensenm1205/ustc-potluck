8f7a3d5e81c9ac401b786b05bd4ffcffb8333e13
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2019-01-18 16:17:10 -0700

    conditional rendering for Items Comp

--- src/components/Items/index.js
+++ src/components/Items/index.js
@@ -3,16 +3,18 @@ import { Items, Title } from './css/items';
 import ItemCard from './Card';
 
 const ItemsContainer = ({ people, remove }) => {
+    if (people.length === 0 || people === []) {
+        return (
+            <Items>
+                <Title>What people are bringing: </Title>
+                <p>No one has signed up!</p>
+            </Items>
+        )
+    }
     return(
         <Items>
             <Title>What people are bringing: </Title>
-            {people.map(personObj => {
-                if (personObj === 0) {
-                    return null
-                }
-
-                return <ItemCard person={personObj} key={personObj.id} remove={remove} />
-            })}
+            {people.map(personObj => <ItemCard person={personObj} key={personObj.id} remove={remove} />)}
         </Items>
     )
 };
