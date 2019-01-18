e8b7426a9b5126ff533af4b28798416a51633acc
Matt Mortensen <mortensenm1205@gmail.com>
2019-01-17 21:17:16 -0700

    seperating css for List comp

--- src/components/List/css/list.js
+++ src/components/List/css/list.js
@@ -8,3 +8,11 @@ export const Title = styled.p`
     font-size: 2rem;
     font-weight: 900;
 `;
+
+export const Section = styled.section`
+    background-color: #fff;
+    height: 250px;
+    box-sizing: border-box;
+    box-shadow: 1px 17px 24px 5px #e0e0e0;
+    padding: 3%;
+`;
