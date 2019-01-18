9b8b8f05067e7485a31732b3654da72d915cb795
Matt Mortensen <mortensenm1205@gmail.com>
2019-01-17 18:54:46 -0700

    broke css down into own dir

---
+++ src/components/Items/css/card.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Card = styled.div`
+    background-color: #fff;
+    height: 250px;
+    width: 25%;
+    display: inline-block;
+    box-sizing: border-box;
+    box-shadow: 1px 17px 24px 5px #e0e0e0;
+    margin: 0 2% 2% 0;
+    padding: 2.5%;
+`;
+
+export const CardName = styled.p`
+    font-weight: 700;
+`;
+
+export const CardItem = styled.p``;
