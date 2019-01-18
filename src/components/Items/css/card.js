2ff8b89bc8e4e7c3ddfe8b04282b00a3ddf93b1a
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2019-01-18 09:51:33 -0700

    small styling change

--- src/components/Items/css/card.js
+++ src/components/Items/css/card.js
@@ -9,7 +9,7 @@ export const Card = styled.div`
     box-sizing: border-box;
     box-shadow: 1px 17px 24px 5px #e0e0e0;
     margin: 0 3% 4% 0;
-    padding: 2.5%;
+    padding: 2.3%;
 `;
 
 export const CardName = styled.p`
