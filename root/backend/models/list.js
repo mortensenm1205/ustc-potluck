ce842dd6f41b06b336c9330931c4207dd7313c79
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2019-01-21 15:50:38 -0700

    posting list item for pl now ready

--- root/backend/models/list.js
+++ root/backend/models/list.js
@@ -1,5 +1,4 @@
-const mongoose = require('mongoose');
-const { Schema, model } = mongoose;
+const { Schema, model } = require('mongoose');
 
 let ListSchema = new Schema({
     name: {
@@ -8,7 +7,7 @@ let ListSchema = new Schema({
         minlength: 1
     },
     item: {
-        type: mongoose.Schema.Types.ObjectId, 
+        type: String,
         required: true,
         minlength: 1
     }
@@ -16,4 +15,4 @@ let ListSchema = new Schema({
 
 let List = model('List', ListSchema)
 
-module.export = { List };
+module.exports = { List };
--- root/backend/routes/list.js
+++ root/backend/routes/list.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const { mongoose } = require('../config/db');
 const { List } = require('../models/list');
+const { Foods } = require('../models/foods');
 
 const router = express.Router();
 
@@ -10,5 +11,20 @@ router.get('/getList', (req, res) => {
         .then(item => res.status(200).send({ item }))
         .catch(e => res.status(400).send(e))
 });
+
+router.post('/addItem', (req, res) => {
+    let item = new List({
+        name: req.body.name,
+        item: req.body.item
+    })
+
+    item.save()
+        .then(it => {
+            Foods.deleteOne({ item: it.item})
+                .then(response => res.status(200).send(response))
+                .catch(e => res.status(400).send(e))
+        })
+        .catch(e => res.status(400).send(e))
+})
 
 module.exports = router;
