5aea9bb7f0c2a9623051f730ad91068add9097ca
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2019-01-22 12:07:34 -0700

    small styling change for Food list

--- root/frontend/src/Foods/css/list.js
+++ root/frontend/src/Foods/css/list.js
@@ -11,8 +11,12 @@ export const Title = styled.p`
 
 export const Section = styled.section`
     background-color: #fff;
-    height: 250px;
+    height: 300px;
     box-sizing: border-box;
     box-shadow: 1px 17px 24px 5px #e0e0e0;
     padding: 3%;
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: column
+    justify-content: flex-start;
 `;
