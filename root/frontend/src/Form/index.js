d4f3611397b75301dc4d88015ad8f26fe6a927e7
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2019-01-22 10:17:00 -0700

    small name changes

--- root/frontend/src/Form/index.js
+++ root/frontend/src/Form/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Form, Input, Button, Title, SubTitle } from './css/form';
 
-const FormContainer = ({ entry, submitEntry }) => {
+const FormContainer = ({ formChange, formSubmit }) => {
     return(
         <Form>
            <Title>Enter your item</Title>
            <SubTitle>Enter your name and the item you're bringing to the potluck.</SubTitle> 
-           <form onSubmit={submitEntry}>
-                <Input type="text" placeholder="Name" name="name" onChange={entry} />
-                <Input type="text" placeholder="Item" name="item" onChange={entry} />
+           <form onSubmit={formSubmit}>
+                <Input type="text" placeholder="Name" name="name" onChange={formChange} />
+                <Input type="text" placeholder="Item" name="item" onChange={formChange} />
                 <Button>Sign up</Button>
            </form>
         </Form>
