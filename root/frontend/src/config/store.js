f9ce5c60b608049ec33d546736c7614de6b1d4d9
Matt Mortensen <mortensenm1205@gmail.com>
2019-01-21 20:34:43 -0700

    creating redux store

---
+++ root/frontend/src/config/store.js
@@ -0,0 +1,18 @@
+import { 
+    createStore, 
+    combineReducers, 
+    compose, 
+    applyMiddleware 
+} from 'redux';
+import thunk from 'redux-thunk';
+
+const rootReducer = combineReducers({
+    
+})
+
+export default createStore(
+    rootReducer,
+    compose(
+        applyMiddleware(thunk)
+    )
+)
