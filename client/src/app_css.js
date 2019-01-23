63b8efb84a02fd59dcb91bb02be8e1893bde4255
Matt Mortensen <mortensenm1205@gmail.com>
2019-01-22 18:53:31 -0700

    uni updating for mobile

--- client/src/app_css.js
+++ client/src/app_css.js
@@ -3,7 +3,13 @@ import styled from 'styled-components'
 export const AppContainer = styled.div`
     display: grid;
     grid-template-columns: repeat(12, 1fr);
-    grid-template-rows: repeat(4, 1fr);
+    grid-template-rows: repeat(3, auto);
     font-size: 18px;
     font-family: 'Roboto', sans-serif;
+    padding: 2.5%;
+    
+    @media(min-width: 800px) {
+        grid-template-rows: repeat(4, auto);
+        padding: 0;
+    }
 `;
