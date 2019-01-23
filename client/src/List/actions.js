b27f32c6391c947d4a66c9f64e3d9061ffd5fbe5
Matt Mortensen <mortensenm1205@gmail.com>
2019-01-22 18:31:21 -0700

    updating form values to lower case.

--- client/src/List/actions.js
+++ client/src/List/actions.js
@@ -26,7 +26,11 @@ export const addPotluckItem = (location, potLuckItem) => {
     return dispatch => {
         axios.post(
             '/api/plList/addPotLuckItem',
-            { name: potLuckItem.name, item: potLuckItem.item })
+            { 
+                name: potLuckItem.name, 
+                item: potLuckItem.item.toLowerCase() 
+            }
+        )
         .then(res => res ? location.reload() : null)
         .catch(e => console.log(e))
     }
@@ -36,7 +40,7 @@ export const removePotluckItem = (location, potLuckItem) => {
     return dispatch => {
         axios.delete(
             '/api/plList',
-            { params: { plItem: potLuckItem.item }})
+            { params: { plItem: potLuckItem.item.toLowerCase() }})
         .then(res => res ? location.reload() : null)
         .catch(e => console.log(e))
     }
