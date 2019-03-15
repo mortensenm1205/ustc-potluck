ed670dada3c0851835398fbc05a760ea670c2d44
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2019-03-15 13:56:29 -0600

    finsihing connection for base s tyles

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app/App';
+import { Base } from './app/css';
 import { Provider } from 'react-redux';
 import store from './app/config/store';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
+    <div>
+        <Base />
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </div>,
     document.getElementById('root')
 );
 
