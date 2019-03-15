847ce5e5e95bed832e206a73a070b40638676181
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2019-03-15 15:53:13 -0600

    more comments

--- client/src/app/Form/index.js
+++ client/src/app/Form/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Form, Input, Button, Title, SubTitle } from './css/form';
+import { customModalStyles, ModalButton } from './css/modal';
 import Modal from 'react-modal';
 
 Modal.setAppElement('#root');
@@ -9,7 +10,8 @@ class FormContainer extends Component {
     state = {
         modalIsOpen: false
     }
-    
+
+    // This allows the modal to open and close. 
     open = () => this.setState({ modalIsOpen: true })
     close = () => this.setState({ modalIsOpen: false })
 
@@ -17,10 +19,13 @@ class FormContainer extends Component {
         const { formChange, formSubmit } = this.props;
         return(
             <div>
-                <button onClick={this.open}>+</button>
+                {/* Using that state here */}
+                <ModalButton onClick={this.open}>+</ModalButton>
                 <Modal
                     isOpen={this.state.modalIsOpen}
                     onRequestClose={this.close}
+                    // Using a custom style to handle height and width
+                    style={customModalStyles}
                 >
                     <Form>
                         <Title>Enter your item</Title>
@@ -31,6 +36,7 @@ class FormContainer extends Component {
                                 <Button>Sign up</Button>
                         </form>
                     </Form>
+                    {/* Just using the same method as above to close. */}
                     <button onClick={this.close}>close modal</button>
                 </Modal>
             </div>
