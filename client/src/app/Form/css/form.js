861c2757503dc1b295e8f080bb18478e507d01b7
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2019-03-15 15:53:02 -0600

    adding comments

--- client/src/app/Form/css/form.js
+++ client/src/app/Form/css/form.js
@@ -1,19 +1,7 @@
 import styled from 'styled-components';
 
 export const Form = styled.div`
-    grid-column: 1 / -1;
-    border: 6px solid #000;
-    background-color: #fff;
-    padding: 1% 4%;
-    box-shadow: 3px 12px 32px -8px #000;
-
-    @media(min-width: 800px) {
-        // grid-column: 4 / 7;
-        // grid-row: 1 / 3;
-        border: 12px solid #000;
-        padding: 3% 4%;
-
-    }
+    @media(min-width: 800px) {}
 `;
 
 export const Title = styled.p`
@@ -23,7 +11,6 @@ export const Title = styled.p`
 `;
 
 export const SubTitle = styled.p`
-    margin: 0 0 10%;
     line-height: 30px;
 `;
 
@@ -31,8 +18,6 @@ export const Input = styled.input`
     display: block;
     width: 80%;
     font-size: 1.4rem;
-    margin: 0 0 5%;
-    padding: 4% 0;
     border: none;
     border-bottom: 2px solid #ccc;
 
