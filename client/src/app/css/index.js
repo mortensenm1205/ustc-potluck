477df742882869e2fd7c1e2f12bab71f98d4935c
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2019-03-15 13:56:11 -0600

    removing base styles out of index and into app css index

--- client/src/app/css/index.js
+++ client/src/app/css/index.js
@@ -1,17 +1,25 @@
-import styled from 'styled-components'
+import styled, { createGlobalStyle } from 'styled-components';
+
+export const Base = createGlobalStyle`
+    html {
+        background: rgb(245,243,243);
+        font-size: 16px;
+        font-family: 'Roboto', sans-serif;
+    }
+`;
 
 export const AppContainer = styled.div`
     display: grid;
     grid-template-columns: repeat(12, 1fr);
     grid-template-rows: repeat(3, auto);
     grid-gap: 15px;
-    font-size: 18px;
-    font-family: 'Roboto', sans-serif;
     padding: 2.5%;
     color: #626262;
     
     @media(min-width: 800px) {
-        grid-template-rows: repeat(4, auto);
+        // Needing the rows to be fixed
+        // For easier placement of comps.
+        grid-template-rows: repeat(3, 100px);
         padding: 0;
     }
 `;
