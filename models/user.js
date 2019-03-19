0fe8ef38189d9fcb270b4bde813207452164506f
Matt Mortensen <mortensenm1205@gmail.com>
2019-03-19 11:46:46 -0600

    added username for easier auth/db config

--- models/user.js
+++ models/user.js
@@ -1,11 +1,28 @@
 const { Schema, model } = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 let UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
     password: {
         type: String, 
-        default: 'ustcPotluck!'
+        required: true
     }
-})
+});
+
+UserSchema.methods.setPassHash = function(pass) {
+    // get and set for salt and hash
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(pass, salt);
+    // updating the password value to the new hash value.
+    return this.password = hash;
+}
+
+UserSchema.statics.comparePassHash = function (entered_pass, saved_pass) {
+    return bcrypt.compareSync(entered_pass, saved_pass);;
+}
 
 let User = model('User', UserSchema);
 
