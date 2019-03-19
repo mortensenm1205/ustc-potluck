cc95d20aae8ceba547e6014fe1fbae76ea072773
Matt Mortensen <mortensenm1205@gmail.com>
2019-03-19 13:37:54 -0600

    writing up rest of action to call on reducer

--- client/src/app/Login/ducks/actions.js
+++ client/src/app/Login/ducks/actions.js
@@ -10,7 +10,10 @@ const loginUserSuccess = userData => {
 export const loginUser = userData => {
     return dispatch => {
         axios.post('/api/user/', { username: userData.username, password: userData.password })
-            .then(res => console.log(res))
+            .then(res => {
+                console.log(res.data);
+                dispatch(loginUserSuccess(res.data))
+            })
             .catch(err => console.error(err))
     }
 }
