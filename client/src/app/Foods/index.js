2642396109c9d25ec1f15b050731af0784455b77
Matt Mortensen <mortensenm1205@gmail.com>
2019-03-19 19:45:21 -0600

    activeUser now displays text area when logged in.

--- client/src/app/Foods/index.js
+++ client/src/app/Foods/index.js
@@ -1,18 +1,33 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
 import { List, Title, Section } from './css/list';
 import FoodItem from './Item';
 
-const FoodContainer = ({ foods }) => {
+const FoodContainer = ({ foods, activeUser }) => {
     return(
         <List>
             <Title>What's left to bring: </Title>
-            <Section>
-                {foods.map(food => {
-                    return <FoodItem key={food._id} food={food} />
-                })}
-            </Section>
+            {foods.map(food => {
+                return activeUser.activeUser ? 
+                <textarea defaultValue={food.item} key={food._id} />
+            :  
+                <Section key={food._id}>
+                    <FoodItem food={food} />
+                </Section>
+            })}
         </List>
     )
 }
 
-export default FoodContainer;
+const mapStateToProps = state => {
+    return {
+        activeUser: state.activeUserBool
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(FoodContainer);
