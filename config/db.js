07e8a4c280c76c0498e51ebe230ad042c4a46f93
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2019-12-04 09:27:17 -0700

    add options for db config

--- config/db.js
+++ config/db.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost:27017/ustc-potluck',
-    { 
-        useNewUrlParser: true, 
-        useCreateIndex: true 
-    },
+  process.env.MONGODB_URI || "mongodb://localhost:27017/ustc-potluck",
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+  }
 );
 
 module.exports = { mongoose };
