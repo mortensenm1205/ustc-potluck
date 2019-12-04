998564ccf019c3a25d140dd677e43bb5187d20ec
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2019-12-04 11:23:08 -0700

    add fdsState for better control, and add textarea

--- client/src/app/Foods/index.js
+++ client/src/app/Foods/index.js
@@ -1,19 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import { List, Title, Section } from './css/list';
 import FoodItem from './Item';
 
-const FoodContainer = ({ foods }) => {
-    return(
-        <List>
+class FoodContainer extends Component {
+
+    state = {
+        editable: false,
+        foodsState: []
+    }
+
+    componentDidUpdate(prevProps) {
+        if(prevProps.foods !== this.props.foods) {
+            this.setState({
+                foodsState: [...this.props.foods]
+            });
+        }
+    }
+
+    editList = () => {
+        this.setState({ 
+            editable: !this.state.editable
+        });
+    }
+
+    render() {
+        const { foodsState, editable } = this.state;
+        return (
+          <List>
             <Title>What's left to bring: </Title>
-            <Section >
-                {foods.map(food => { 
-                    return <FoodItem food={food} key={food._id}/>
-                })}
+            <Section>
+              {foodsState.map(food => {
+                if (editable) {
+                  return <textarea value={food.item} key={food._id}></textarea>;
+                }
+                return <FoodItem food={food} key={food._id} />;
+              })}
             </Section>
-        </List>
-    )
+            <button onClick={this.editList}>Edit List</button>
+          </List>
+        );
+    }
 }
+
 
 export default FoodContainer;
