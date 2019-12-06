31e0adfa98b555af8880801270cbdd7b11f151ca
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2019-12-06 09:31:59 -0700

    bringing in close btn for foodItem

--- client/src/app/Foods/Item.js
+++ client/src/app/Foods/Item.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import { Food } from './css/food';
+import { Food, FoodDetails, RemoveFoodBtn } from "./css/food";
 
-const FoodItem = ({ food }) => {
-    return <Food>{food.item}</Food>
+const FoodItem = ({ food, editable }) => {
+    return (
+      <Food>
+        {editable && <RemoveFoodBtn>X</RemoveFoodBtn>}
+        <FoodDetails>{food.item}</FoodDetails>
+      </Food>
+    );
 }
 
 export default FoodItem;
