c7f1dc7ab5bfbaf61f1409dffd5af1b2864f8e11
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2019-12-06 09:32:12 -0700

    css to match the foodItem close btn

--- client/src/app/Foods/css/food.js
+++ client/src/app/Foods/css/food.js
@@ -1,6 +1,39 @@
 import styled from 'styled-components';
 
-export const Food = styled.p`
+export const Food = styled.div`
+    position: relative;
+`;
+
+export const FoodDetails = styled.p`
     font-size: 1.3rem;
     margin: 2.5% 0;
 `; 
+
+export const RemoveFoodBtn = styled.button`
+    position: absolute;
+    top: 10px;
+    left: 220px;
+    border: none;
+    font-size: 1.6rem;
+    background-color: #fff;
+    padding: 1% 2%;
+
+    :hover {
+        background-color: #f93e3e;
+        color: #fff;
+        border: 0.5px solid #757575;
+        box-shadow: 9px 7px 14px -9px #000;
+    }
+
+    :active {
+        transform: translateY(2px);
+    }
+
+    :focus {
+        outline: 0;
+    }
+
+    @media (min-width: 800px) {
+        left: 344px;
+    }
+`;
