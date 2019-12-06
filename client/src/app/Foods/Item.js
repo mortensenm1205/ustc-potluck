2ae9996c88484c61595aa7de1028dca5425e1c36
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2019-12-06 13:35:26 -0700

    food item comp sends back indv index to food comp

--- client/src/app/Foods/Item.js
+++ client/src/app/Foods/Item.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Food, FoodDetails, RemoveFoodBtn } from "./css/food";
 
-const FoodItem = ({ food, editable, remove }) => {
+const FoodItem = ({ food, editable, remove, index }) => {
     return (
       <Food>
-        {editable && <RemoveFoodBtn onClick={remove}>X</RemoveFoodBtn>}
+        {editable && 
+            <RemoveFoodBtn 
+             onClick={() => remove(index)}>
+                 X
+            </RemoveFoodBtn>}
         <FoodDetails>{food.item}</FoodDetails>
       </Food>
     );
