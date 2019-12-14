85709f867f56ba21612432354d5b2c403f463ee9
Matt Mortensen <mortensenm1205@gmail.com>
2019-12-13 19:06:13 -0700

    comment and condition clause for empty arr

--- client/src/app/List/index.js
+++ client/src/app/List/index.js
@@ -3,6 +3,14 @@ import { Items, Title } from './css/items';
 import ListCard from './Card';
 
 const ListContainer = ({ people, remove }) => {
+    /*
+        When you remove all items from the people array
+        the array turns to undefined which crashes the whole 
+        program. This condition just updates the undefined 
+        back to an empty array.
+    */
+    people = people === undefined ? [] : people;
+
     return(
         <Items>
             <Title>What people are bringing: </Title>
