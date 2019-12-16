5c89ef263ebb8dab06fc1b08749f661ad4184e2d
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2019-12-16 15:34:28 -0700

    foodListBtn, foodStyleCont, other comps now done

--- client/src/app/Foods/css/food.js
+++ client/src/app/Foods/css/food.js
@@ -1,13 +1,44 @@
 import styled from 'styled-components';
 
-export const Food = styled.div`
+export const FoodStyleContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: column
+    justify-content: flex-start;
+    height: 75%;
+`;
+
+export const FoodDetailsContainer = styled.div`
     position: relative;
+    margin: 1.5% 0;
 `;
 
 export const FoodDetails = styled.p`
     font-size: 1.3rem;
-    margin: 2.5% 0;
 `; 
+
+export const FoodListButton = styled.button`
+    font-size: 1.2rem;
+    background-color: #0475cef5;
+    color: #fff;
+    border: none;
+    border-radius: 20px;
+    margin: 2.5% 0;
+    padding: 3% 2.5%;
+    width: 100%;
+
+    :hover {
+        background-color: #0761a9f5;
+    }
+
+    :focus {
+        outline: 0;
+    }
+
+    @media (min-width: 800px) {
+        width: 150px;
+    }
+`;
 
 export const RemoveFoodBtn = styled.button`
     position: absolute;
