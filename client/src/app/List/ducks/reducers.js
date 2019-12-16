1c5a2ba5b5034116ffec6666f95c6bad3400d721
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2019-12-16 16:55:55 -0700

    pL error obj includes initial bool to false

--- client/src/app/List/ducks/reducers.js
+++ client/src/app/List/ducks/reducers.js
@@ -1,4 +1,4 @@
-export const potluckData = (state = { data: [], errorMessage: { remove: null }}, action) => {
+export const potluckData = (state = { data: [], errorMessage: { remove: false }}, action) => {
     switch(action.type) {
         case "LOAD_POTLUCK_LIST_SUCCESS":
             return {
