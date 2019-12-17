6464a4b717e4b46856e784843f1c530178d13690
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2019-12-17 14:50:24 -0700

    adding cmmnts thoughout proj for later use

--- index.js
+++ index.js
@@ -10,10 +10,13 @@ const list = require('./routes/list');
 const app = express();
 const port = process.env.PORT || 5000;
 
+// This middleware is helping connect CRA to our backend
 app.use(express.static(path.join(__dirname, 'client/build')));
+// Commone middleware utils for JSON and CORS
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json(), cors());
+// Our routes. Objects are part of the Express.router
 app.use('/api/foods', foods);
 app.use('/api/plList', list);
-
+// Standard listening function
 app.listen(port, () => console.log(`Running on port ${port}`))
