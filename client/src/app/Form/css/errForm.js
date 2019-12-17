8354eadbb34b6484434e1f9e73e591c17011818c
Matt Mortensen <mortensenm1205@gmail.com>
2019-12-16 22:14:39 -0700

    updated bck clr

--- client/src/app/Form/css/errForm.js
+++ client/src/app/Form/css/errForm.js
@@ -5,7 +5,8 @@ export const ErrContainer = styled.div`
     top: 65px;
     left: 400px;
     font-size: 1.3em;
-    background-color: red;
+    background-color: #ff3f3f;
     color: white;
     padding: 1% 2.5%;
+    border-radius: 25px;
 `;
