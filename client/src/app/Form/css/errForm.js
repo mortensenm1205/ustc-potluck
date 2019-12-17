e0c7d6ace09db7a90f42d506eb34aae1a78c0d32
Matt Mortensen <mortensenm1205@gmail.com>
2019-12-16 22:00:05 -0700

    styling err message for empty form comp

---
+++ client/src/app/Form/css/errForm.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const ErrContainer = styled.div`
+    position: absolute;
+    top: 65px;
+    left: 400px;
+    font-size: 1.3em;
+    background-color: red;
+    color: white;
+    padding: 1% 2.5%;
+`;
