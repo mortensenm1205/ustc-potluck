d084872d133a42369cde6f8f9636457e052f04c1
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2019-12-17 15:05:47 -0700

    comments on food route

--- routes/foods.js
+++ routes/foods.js
@@ -2,16 +2,19 @@ const express = require('express');
 
 const { mongoose } = require('../config/db');
 const { Food } = require('../models/foods');
-
+// Here i'm using express router that is then exported and used in my main index.js
 const router = express.Router();
 
+// This route is just getting data from my Food model
+// And sending the food array back to the client request
 router.get('/getFoods', (req, res) => {
     Food.find()
         .then(food => res.status(200).send({ food }))
         .catch(e => res.status(400).send(e))
 });
 
-// This will become a protected route later on.
+// This route is adding food obj's to our Food model
+// We then send the saved food obj back to the client 
 router.post('/addFood', (req, res) => {
     // Str mutation the uppercases first letter of req.body.item and applies to more than one value
     let { item } = req.body;
@@ -26,10 +29,13 @@ router.post('/addFood', (req, res) => {
     })
 
     food.save()
-        .then(food => res.status(200).send({ food }))
+        .then(food => res.status(200).send({ food })) // This 'food' is being sent back to the client
         .catch(e => res.status(400).send(e))
 });
 
+// And if we need to delete any existing Food items 
+// Then we use this route. It just sends back a deleted response
+// which i believe we don't use on the client side
 router.delete(':foodItem?', (req, res) => {
     Food.deleteOne({ item: req.query.foodItem })
         .then(deleted_response => res.status(200).send(deleted_response))
